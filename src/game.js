a4bac55fabb9582a381b5fb923399d82152abd3a
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2020-06-18 18:35:27 -0700

    files made

---
+++ src/game.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Game = () => {
+
+    return (
+        <div>
+            <h1> Verbose Game of Life</h1>
+            {/* <Grid /> */}
+            <h2>Generations: </h2>
+
+        </div>
+    )
+
+}
+export default Game
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+import Game from './game'
+
 ReactDOM.render(
   <React.StrictMode>
     <Game />
