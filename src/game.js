491921353e2de8a98ea0b57c39aa16bbb6d76800
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2020-06-21 11:01:07 -0700

    grid up just black

--- src/game.js
+++ src/game.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Grid from "./grid";
 
+const CELL_SIZE = 20;
+const WIDTH = 800;
+const HEIGHT = 600;
+
 const Game = () => {
   return (
     <div>
-      <h1> Verbose Game of Life</h1>
-      <Grid />
-      <h2>Generations: </h2>
+      <div className="board" style={{ width: WIDTH, height: HEIGHT }}></div>
     </div>
   );
 };
