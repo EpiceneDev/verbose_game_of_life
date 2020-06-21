636467d6a850e40916ae29a5c725a835de2a02cd
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2020-06-21 11:30:03 -0700

    make empty board function done

--- src/game.js
+++ src/game.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Grid from "./grid";
 import "./game.css";
 
@@ -7,6 +7,26 @@ const WIDTH = 800;
 const HEIGHT = 600;
 
 const Game = () => {
+  let rows = HEIGHT / CELL_SIZE;
+  let cols = WIDTH / CELL_SIZE;
+
+  const [state, setState] = useState({
+    cells: [],
+  });
+
+  const makeEmptyBoard = () => {
+    let board = [];
+    for (let y = 0; y < rows; y++) {
+      board[y] = [];
+      for (let x = 0; x < cols; x++) {
+        board[y][x] = false;
+      }
+    }
+    return board;
+  };
+
+  let board = makeEmptyBoard();
+
   return (
     <div>
       <div
