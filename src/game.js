71a2665207f59dbf6d1317f6452babfca128e3a5
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2020-06-21 11:20:19 -0700

    grid up with cells showing

--- src/game.js
+++ src/game.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Grid from "./grid";
+import "./game.css";
 
 const CELL_SIZE = 20;
 const WIDTH = 800;
@@ -8,7 +9,14 @@ const HEIGHT = 600;
 const Game = () => {
   return (
     <div>
-      <div className="board" style={{ width: WIDTH, height: HEIGHT }}></div>
+      <div
+        className="board"
+        style={{
+          width: WIDTH,
+          height: HEIGHT,
+          backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`,
+        }}
+      ></div>
     </div>
   );
 };
