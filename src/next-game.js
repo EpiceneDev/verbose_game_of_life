98fad9ee8650a8d943d2784280e92ffb80536aee
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2020-06-22 12:01:54 -0700

    board grid made

--- src/next-game.js
+++ src/next-game.js
@@ -5,6 +5,7 @@ const rows = 50;
 const cols = 50;
 
 // TODO may try Math.floor(Math.random()*4)
+// 0.3 gives a 30% chance of being alive
 const newBoardStatus = (cellStatus = Math.random() < 0.3) => {
   const grid = [];
 
@@ -17,3 +18,40 @@ const newBoardStatus = (cellStatus = Math.random() < 0.3) => {
   }
   return grid;
 };
+
+// Creates the board grid and assigns it to a variable
+// and a method allowing users to toggle the status of individual cells as props. 
+// This method is defined on the main component where all the state of the application is held.
+const BoardGrid = ({ boardStatus, onToggleCellStatus }) => {
+    const handleClick = (r,c) => onToggleCellStatus(r,c);
+
+    const tr = [];
+    for (let r = 0; r < totalBoardRows; r++) {
+        const td = [];
+        for (let c = 0; c < totalBoardColumns; c++) {
+            td.push(
+                <td
+                    key={`${r},${c}`}
+                    className={boardStatus[r][c] ? 'alive' : 'dead'}
+                    onClick={() => handleClick(r,c)}
+                />
+            );
+        }
+        tr.push(<tr key={r}>{td}</tr>);
+    }
+    return <table><tbody>{tr}</tbody></table>;
+}
+
+const Slider = () => {}
+
+function Game() {
+    const [gameState, setGameState] = useState({
+
+    })
+
+    return (
+
+    )
+}
+
+export default Game
