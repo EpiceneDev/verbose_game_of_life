14efe2815565c129c50c7c01ac635d66a0bdd07d
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2020-06-24 11:43:03 -0700

    generator done

--- src/game.js
+++ src/game.js
@@ -4,7 +4,9 @@ import produce from "immer";
 const numRows = 25;
 const numCols = 25;
 
-const operations = [
+const speed = 100;
+
+const neighborLoc = [
   [0, 1],
   [0, -1],
   [1, -1],
@@ -17,33 +19,72 @@ const operations = [
 
 // Helper function
 const generateEmptyGrid = () => {
-    const rows = []
-    for (let i = 0; i < numRows; i++) {
-        rows.push(Array.from(Array(numCols), () => 0))
-    }
-    return rows;
-}
+  const rows = [];
+  for (let i = 0; i < numRows; i++) {
+    rows.push(Array.from(Array(numCols), () => 0));
+  }
+  return rows;
+};
 
 function Game() {
-    // Put grid into state for access
-    const [grid, setGrid] = useState({
-        return generateEmptyGrid();
+  // The grid is the displayed/video buffer (running buffer)
+  // First, create a grid and use it
+  // as initial value for the grid state/video buffer
+  const [grid, setGrid] = useState(() => {
+    return generateEmptyGrid();
+  });
+
+  // Set flags for running
+  const [isRunning, setIsRunning] = useState(false);
+
+  // and a ref that persists without causing rerender.
+  const runRef = useRef(isRunning);
+
+  runRef.current = isRunning;
+
+  // Rerender the the grid based on the runRef flag
+  const runSimulation = useCallback(() => {
+    if (!runRef.current) {
+      return;
+    }
+
+    // ⬇️ Examples from immer on Github
+    // produce(currentState, producer: (draftState) => void): nextState
+    // const mapper = produce((draft, index) => {
+    //     draft.index = index
+    // })
+
+    // produce is a currying technique
+    // Currying takes multiple arity of function and
+    //   calls again with less functions as a means of
+    //   cellular autonama (CA)
+
+    // setGrid will build the map
+    setGrid((g) => {
+      return produce(g, (gridCopy) => {
+        for (let i = 0; i < numRows; i++) {
+          for (let j = 0; j < numCols; j++) {
+            let neighbors = 0;
+            neighborLoc.forEach(([x, y]) => {
+              const newI = i + x;
+              const newJ = j + y;
+              if (newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols) {
+                neighbors += g[newI][newJ];
+              }
+            });
+
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][j] = 0;
+            } else if (g[i][j] === 0 && neighbors === 3) {
+              gridCopy[i][j] = 1;
+            }
+          }
+        }
+      });
     });
 
-    // Set flags for running and a ref that persists without causing rerender
-    const [isRunning, setIsRunning] = useState(false)
-
-    const runRef = useRef(isRunning);
-
-    runRef.current = isRunning;
-
-    const 
-
-    setGrid(() => {
-        returns produce(draft, (gridCopy) => {
-            
-        })
-    })
+    setTimeout(runSimulation, 100);
+  }, []);
 }
 
-export default Game
+export default Game;
