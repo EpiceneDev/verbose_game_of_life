aee4c81cf40197754f2cce296d2b6b4b521ac1e8
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2020-06-24 12:58:17 -0700

    fixed run interruptions

--- src/game.js
+++ src/game.js
@@ -27,17 +27,18 @@ const generateEmptyGrid = () => {
 };
 
 const Slider = (speed, onSpeedChange) => {
-    const handleChange = (e) => onSpeedChange(e.target.value)
-    return (
-        <input 
-            type="range"
-            min="50"
-            max="1500"
-            step="50"
-            value={speed}
-            onChange={handleChange}
-    )
-}
+  const handleChange = (e) => onSpeedChange(e.target.value);
+  return (
+    <input
+      type="range"
+      min="50"
+      max="1500"
+      step="50"
+      value={speed}
+      onChange={handleChange}
+    />
+  );
+};
 
 function Game() {
   // The grid is the displayed/video buffer (running buffer)
@@ -50,7 +51,9 @@ function Game() {
   // Set flags for running
   const [isRunning, setIsRunning] = useState(false);
 
-  const [speed, setSpeed] = useState("50")
+  const [speed, setSpeed] = useState("50");
+
+  const [generation, setGeneration] = useState("0");
 
   // and a ref that persists without causing rerender.
   const runRef = useRef(isRunning);
@@ -102,14 +105,18 @@ function Game() {
   }, []);
 
   const handleRun = () => {
-    setRunning(!running);
-    if (!running) {
+    setIsRunning(!isRunning);
+    if (!isRunning) {
       runRef.current = true;
+      setGeneration(generation + 1);
       runSimulation();
     }
   };
 
   const handlePopulate = () => {
+    if (isRunning) {
+      return;
+    }
     const rows = [];
     for (let i = 0; i < numRows; i++) {
       rows.push(
@@ -125,20 +132,50 @@ function Game() {
   };
 
   const speedChangeHandler = (newSpeed) => {
-    setSpeed(newSpeed)
-  }
+    setSpeed(newSpeed);
+  };
 
   return (
-    <div className="board">
-      <button onClick={handleRun}>{isRunning ? start : stop}</button>
-      <button onClick={handlePopulate}>Populate</button>
-      <button onClick={handleClear}>Clear</button>
-      <span>
-        {"FASTER "}
-        <slider speed={speed} onChange={speedChangeHandler} />
-        {" SLOWER"}
-      </span>
-    </div>
+    <>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: `repeat(${numCols}, 20px)`,
+        }}
+      >
+        {grid.map((rows, i) =>
+          rows.map((col, k) => (
+            <div
+              key={`${i}-${k}`}
+              onClick={() => {
+                const newGrid = produce(grid, (gridCopy) => {
+                  gridCopy[i][k] = grid[i][k] ? 0 : 1;
+                });
+                setGrid(newGrid);
+              }}
+              style={{
+                width: 20,
+                height: 20,
+                backgroundColor: grid[i][k] ? "pink" : undefined,
+                border: "solid 1px black",
+              }}
+            />
+          ))
+        )}
+      </div>
+      <div className="buttons">
+        <button onClick={handleRun}>{isRunning ? "stop" : "start"}</button>
+        <button onClick={handlePopulate}>Populate</button>
+        <button onClick={handleClear}>Clear</button>
+        <div className="slider">
+          <span>
+            {"FASTER "}
+            <slider speed={speed} onChange={speedChangeHandler} />
+            {" SLOWER"}
+          </span>
+        </div>
+      </div>
+    </>
   );
 }
 
